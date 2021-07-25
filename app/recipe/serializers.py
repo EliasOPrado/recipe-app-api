13bceacc36dbe10d5a7635a83c0d16886bced503
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-07-25 19:42:23 -0300

    feat: Added list recipe feature.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
     """ Serializer for tag objects """
+
     class Meta:
         model = Tag
         fields = ['id', 'name']
@@ -12,7 +13,26 @@ class TagSerializer(serializers.ModelSerializer):
 
 class IngredientSerializer(serializers.ModelSerializer):
     """ Serializer for ingredient objects """
+
     class Meta:
         model = Ingredient
         fields = ['id', 'user', 'name']
+        read_only_fields = ['id']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """ Serializer for recipe objects """
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'ingredients', 'tags', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -5,6 +5,7 @@ from recipe import views
 router = routers.DefaultRouter()
 router.register('tags', views.TagViewSet, basename='tag')
 router.register('ingredients', views.IngredientViewSet, basename='ingredients')
+router.register('recipes', views.RecipeViewSet, basename='recipes')
 
 app_name = 'recipe'
 
