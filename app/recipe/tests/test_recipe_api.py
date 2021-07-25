a006ad500033bd2bcdef8416c7432744ca8f1e12
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-07-25 20:10:54 -0300

    feat: add tests and implemented feature for retrieving recipe detail

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -5,11 +5,26 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 RECIPES_URL = reverse('recipe:recipes-list')
+
+
+def detail_url(recipe_id):
+    """ return recipe detail url"""
+    return reverse('recipe:recipes-detail', args=[recipe_id])
+
+
+def sample_tag(user, name='Main course'):
+    """ Create and return a sample tag """
+    return Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name='Cinnamon'):
+    """ Create and return a sample ingredient"""
+    return Ingredient.objects.create(user=user, name=name)
 
 
 def sample_recipe(user, **params):
@@ -38,6 +53,7 @@ class PublicRecipeApiTests(TestCase):
 
 class PrivateRecipeApiTests(TestCase):
     """ Test unalthenticated recipe API access """
+
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
@@ -75,3 +91,15 @@ class PrivateRecipeApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
+
+    def test_view_recipe_detail(self):
+        """ test viewing a recipe detail """
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data, serializer.data)
