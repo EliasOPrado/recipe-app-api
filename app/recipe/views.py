60bcfadef6502a380b640de3476f6659f5848e8d
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-07-25 22:36:34 -0300

    feat: added image upload feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets, mixins
-from .serializers import TagSerializer, IngredientSerializer, RecipeSerializer, RecipeDetailSerializer
+from rest_framework import viewsets, mixins, status
+from .serializers import TagSerializer, IngredientSerializer, RecipeSerializer, RecipeDetailSerializer, RecipeImageSerializer
 from core.models import Tag, Ingredient, Recipe
 
 
@@ -37,23 +38,60 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    """ Manage Recipes in database """
+    """Manage recipes in the database"""
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
-        """ Retrieve the recipes for the authenticated user """
-        return self.queryset.filter(user=self.request.user)
+        """Retrieve the recipes for the authenticated user"""
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
-        """ Return appropriated serializer class"""
+        """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return RecipeImageSerializer
+
         return self.serializer_class
 
     def perform_create(self, serializer):
-        """ Create new recipe """
-        return serializer.save(user=self.request.user)
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
